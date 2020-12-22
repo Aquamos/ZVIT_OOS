8e752dc4b5cef25a470fcc23cc18cbf69fdc6864
Badenko Danylo <ftmerlindaniel2017@gmail.com>
2020-12-22 23:11:25 +0200

    Add my script

---
+++ myscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/xpams/UIView-Blur/master/README.md
+echo "The first 11 lines of the file README.md:"
+head -n11 /home/badenko/Desktop/Projects/README.md
+rm /home/badenko/Desktop/Projects/README.md
