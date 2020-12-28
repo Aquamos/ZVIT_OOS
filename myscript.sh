093a29e141d4ecb28f3c8888640cbddcc14ae3c3
Aquamos <47278660+Aquamos@users.noreply.github.com>
2020-12-28 10:51:57 +0200

    Improving functionality
    added the ability to run a script from any folder

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/xpams/UIView-Blur/master/README.md
+echo "Start downloading of README.md........"
+
+thisfile="README.md"
+thisdirectory="./Documents/"
+thislink="https://raw.githubusercontent.com/xpams/UIView-Blur/master/README.md"
+wget -P "$thisdirectory" "$thislink"
 echo "The first 11 lines of the file README.md:"
-head -n11 /home/badenko/Desktop/Projects/README.md
-rm /home/badenko/Desktop/Projects/README.md
+head -n11 "$thisdirectory$thisfile"
+echo "The script has finished running...."
+
+rm "$thisdirectory$thisfile"myscript.sh
